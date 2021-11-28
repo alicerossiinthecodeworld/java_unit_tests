f5a9233eb3cd189ac92040da8eb4b788e9e6d402
alicerossiinthecodeworld <94318766+alicerossiinthecodeworld@users.noreply.github.com>
2021-11-28 17:36:17 +0300

    Revert "Sprint_2"

--- src/main/java/com/example/Feline.java
+++ src/main/java/com/example/Feline.java
@@ -3,6 +3,7 @@ package com.example;
 import java.util.List;
 
 public class Feline extends Animal implements Predator {
+
     @Override
     public List<String> eatMeat() throws Exception {
         return getFood("Хищник");
